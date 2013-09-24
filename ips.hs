3612ed460c7389331fbd92e72e57e367c412e716
spencerwi <spencerwi@gmail.com>
2013-09-24 00:00:28 -0400

    fmap over getNetworkInterfaces; made formatInterfaces point-free

--- ips.hs
+++ ips.hs
@@ -2,11 +2,11 @@ import qualified Network.Info
 import qualified Data.List
 
 extractIpV4 :: Network.Info.NetworkInterface -> String -- formatted like "interface_name: ipv4_addr"
-extractIpV4 iface = (Network.Info.name iface) ++ ": " ++ (show (Network.Info.ipv4 iface))
+extractIpV4 iface = (Network.Info.name iface) ++ ": " ++ ((show . Network.Info.ipv4) iface)
 
 formatInterfaces :: [Network.Info.NetworkInterface] -> String -- format each interface and separate results by newlines
-formatInterfaces ifaceList = Data.List.intercalate "\n" (map extractIpV4 ifaceList)
+formatInterfaces = (Data.List.intercalate "\n") . map extractIpV4
 
 main = do
-    addrs <- Network.Info.getNetworkInterfaces
-    putStrLn $ formatInterfaces addrs
+    addrs <- fmap formatInterfaces Network.Info.getNetworkInterfaces
+    putStrLn $ addrs
